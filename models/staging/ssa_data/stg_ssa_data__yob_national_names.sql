6239b60c0d99ea599f5b9c8f09c2e1d8278a27c9
mickysteinberg@gmail.com <mickysteinberg@gmail.com>
2025-08-23 00:21:08 +0000

    Cleanup staging tables for SSA data

--- models/staging/ssa_data/stg_ssa_data__yob_national_names.sql
+++ models/staging/ssa_data/stg_ssa_data__yob_national_names.sql
@@ -1,6 +1,6 @@
 select
     sex,
     name,
-    births,
+    num_births,
     regexp_extract(_file_name, r'yob(\d{4})\.txt') as year
- from {{ source('ssa_names', 'us_name_births_by_year') }}
+ from {{ source('ssa_data', 'yob_national_names') }}
